5664f26a980cd5c95829a5062989efb6014844ce
Ritesh16 <ritesh.sharma.cse@gmail.com>
2020-12-21 19:21:19 -0500

    Adding inbox/outbox and chat thread

--- API/DTOs/MessageDto.cs
+++ API/DTOs/MessageDto.cs
@@ -6,7 +6,7 @@ namespace API.DTOs
     {
         public int Id { get; set; }
         public int SenderId { get; set; }
-        public string SendUserName { get; set; }
+        public string SenderUserName { get; set; }
         public string SenderPhotoUrl { get; set; }
         public int RecipientId { get; set; }
         public string RecipientUserName { get; set; }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -10,6 +10,7 @@ import { UserParams } from '../_models/userParams';
 import { take } from 'rxjs/operators';
 import { AccountService } from './account.service';
 import { User } from '../_models/user';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 
 @Injectable({
@@ -48,7 +49,7 @@ export class MembersService {
       return of(response);
     }
 
-    let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
+    let params = getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
     params = params.append('minAge', userParams.minAge.toString());
     params = params.append('maxAge', userParams.maxAge.toString());
@@ -56,7 +57,7 @@ export class MembersService {
     params = params.append('orderBy', userParams.orderBy.toString());
 
     var url = this.baseUrl + 'users';
-    return this.getPaginatedResult<Member[]>(url, params)
+    return getPaginatedResult<Member[]>(url, params, this.http)
       .pipe(map(response => {
         this.memberCache.set(Object.values(userParams).join('-'), response);
         return response;
@@ -97,35 +98,9 @@ export class MembersService {
   }
 
   getLikes(predicate: string, pageNumber: number, pageSize: number) {
-    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    let params = getPaginationHeaders(pageNumber, pageSize);
     params = params.append('predicate', predicate);
 
-    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes' , params);
-  }
-
-  private getPaginatedResult<T>(url, params: HttpParams) {
-    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
-
-    return this.http.get<T>(url, { observe: 'response', params }).pipe(
-      map(response => {
-        paginatedResult.result = response.body;
-        if (response.headers.get('Pagination') !== null) {
-          paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
-        }
-
-        return paginatedResult;
-      })
-    );
-  }
-
-  private getPaginationHeaders(pageNumber: number, pageSize: number) {
-    let params = new HttpParams();
-
-    if (pageNumber !== null && pageSize !== null) {
-      params = params.append('pageNumber', pageNumber.toString());
-      params = params.append('pageSize', pageSize.toString());
-    }
-
-    return params;
+    return getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes' , params, this.http);
   }
 }
