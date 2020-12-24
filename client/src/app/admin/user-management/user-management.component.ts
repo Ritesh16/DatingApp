32cc693e6bf755fc6f0e276b67c88ffc3803938f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2020-12-24 16:05:27 -0500

    Adding Admin UI to update roles

---
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { RolesModalComponent } from 'src/app/modals/roles-modal/roles-modal.component';
+import { User } from 'src/app/_models/user';
+import { AdminService } from 'src/app/_services/admin.service';
+
+@Component({
+  selector: 'app-user-management',
+  templateUrl: './user-management.component.html',
+  styleUrls: ['./user-management.component.css']
+})
+export class UserManagementComponent implements OnInit {
+  users: Partial<User[]>;
+  bsModalRef: BsModalRef;
+
+  constructor(private adminSerive: AdminService, private modalService: BsModalService) { }
+
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    this.adminSerive.getUsersWithRoles().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  openRolesModal(user: User) {
+    const config = {
+      class: 'modal-dialog-center',
+      initialState: {
+        user,
+        roles: this.getRolesArray(user)
+      }
+    };
+
+    this.bsModalRef = this.modalService.show(RolesModalComponent, config );
+    //this.bsModalRef.content.closeBtnName = 'Close';
+    this.bsModalRef.content.updateSelectedRoles.subscribe(values => {
+      const rolesToUpdate = {
+        roles: [...values.filter(el => el.checked === true).map(el => el.name)]
+      };
+
+      if(rolesToUpdate){
+        this.adminSerive.updateUserRoles(user.userName, rolesToUpdate.roles).subscribe(() => {
+          user.roles = [...rolesToUpdate.roles];
+        })
+      }
+    });
+  }
+
+  private getRolesArray(user) {
+    const roles = [];
+    const userRoles = user.roles;
+
+    const availableRoles: any[] = [
+      { name: 'Admin', value: 'Admin' },
+      { name: 'Moderator', value: 'Moderator' },
+      { name: 'Member', value: 'Member' }
+    ];
+
+    availableRoles.forEach(role => {
+      let isMatch = false;
+      for (const userRole of userRoles) {
+        if (userRole === role.name) {
+          isMatch = true;
+          role.checked = true;
+          roles.push(role);
+          break;
+        }
+      }
+
+      if(!isMatch){
+        role.checked = false;
+        roles.push(role);
+      }
+    });
+
+    return roles;
+  }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -28,6 +28,9 @@ import { DateInputComponent } from './_forms/date-input/date-input.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 @NgModule({
   declarations: [
@@ -49,7 +52,10 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     DateInputComponent,
     MemberMessagesComponent,
     AdminPanelComponent,
-    HasRoleDirective
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent
   ],
   imports: [
     BrowserModule,
